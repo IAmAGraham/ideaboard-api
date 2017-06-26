00b6cf6176d077123c4fa2f81e0d8aa0b2aa1d54
github username <github email address>
2017-06-25 20:55:36 -0400

    create standard api

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :boards, only: [:index, :create, :update, :destroy]
+      post '/auth', to: 'auth#create'
+      get '/current_user', to: 'auth#show'
+    end
+  end
+end
---
+++ db/migrate/20170626004630_create_boards.rb
@@ -0,0 +1,11 @@
+class CreateBoards < ActiveRecord::Migration[5.1]
+  def change
+    create_table :boards do |t|
+      t.string :title
+      t.string :description
+      t.string :metatag
+
+      t.timestamps
+    end
+  end
+end
